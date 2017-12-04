e0fb65479692ef4e4c4c82c2290eb0af705b478d
nicolo11 <34025832+nicolo11@users.noreply.github.com>
2017-12-04 11:03:24 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : <nn>
+ Fichier     : <nom du fichier>.cpp
+ Auteur(s)   : <prénom> <nom>
+ Date        : <jj.mm.aaaa>
+
+ But         : <à compléter>
+
+ Remarque(s) : <à compléter>
+
+ Compilateur : MinGW-g++ <5.3.0>
+ -----------------------------------------------------------------------------------
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   return EXIT_SUCCESS;
+}
